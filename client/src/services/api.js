9038d64202ca2a19f55102438c003b86d5a0d0b2
Apoorva Raj Srivastava <aapoorva.urvi1112@gmail.com>
2025-09-18 20:30:30 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
-
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+const API_BASE_URL = "https://chatbot-3-c1kz.onrender.com"
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
