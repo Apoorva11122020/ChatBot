8361beb75be86b40eb45888fd14dec939d72e9ba
Apoorva Raj Srivastava <aapoorva.urvi1112@gmail.com>
2025-09-18 21:19:42 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,7 @@ app.use(helmet());
 //     origin: 'https://chat-bot-kappa-tawny.vercel.app',
 //     credentials: true
 // }));
-const allowedOrigins = 'https://chatbot-frontend-8s5i.onrender.com,http://localhost:5173,http://localhost:3000'
+const allowedOrigins = 'https://chatbot-frontend-8s5i.onrender.com'
     .split(',')
     .map(o => o.trim())
     .filter(Boolean);
